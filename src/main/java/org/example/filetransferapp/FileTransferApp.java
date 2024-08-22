bbf7767aaef9f0126c09b230370dca029526411f
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-08-23 00:31:30 +0300

    Add controller for FileTransferApp

---
+++ src/main/java/org/example/filetransferapp/FileTransferApp.java
@@ -0,0 +1,70 @@
+package org.example.filetransferapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.StageStyle;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import javafx.application.Platform;
+
+public class FileTransferApp extends Application {
+
+
+    private ExecutorService executorService = Executors.newCachedThreadPool();
+
+    private File selectedFile;
+    private String saveDirectory;
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+
+
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("FileTransferAppDemo.fxml")));
+            primaryStage.initStyle(StageStyle.TRANSPARENT);
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            FileServer server = new FileServer(5555, FileTransferAppController.getPath());
+            new Thread(server::start).start();
+
+
+//            Запуск сервера для получения файлов
+
+
+
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to load FXML file. Make sure the path is correct.");
+        }
+    }
+
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/org/example/filetransferapp/FileTransferAppController.java
@@ -0,0 +1,82 @@
+package org.example.filetransferapp;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.nio.file.Path;
+
+public class FileTransferAppController {
+    @FXML
+    private Button BtnMin;
+    @FXML
+    private Button BtnClose;
+    @FXML Button BtnChoose;
+
+    @FXML
+    private Pane TopPane;
+    @FXML
+    private static Text PathText;
+    private double xOffset = 0;
+    private double yOffset = 0;
+    private Stage stage;
+    public static String getPath() {
+        return PathText.getText();
+    }
+
+    @FXML
+    protected void handleActionClose(ActionEvent event) {
+        //Делаем стоп серверу
+
+        Stage stage = (Stage) BtnClose.getScene().getWindow();
+        stage.close();
+    }
+    @FXML
+    protected void handleMinAction(ActionEvent event) {
+        Stage stage = (Stage) BtnMin.getScene().getWindow();
+        stage.setIconified(true);
+    }
+    @FXML
+    protected void handleClickAction(MouseEvent event) {
+        Stage stage = (Stage) TopPane.getScene().getWindow();
+        xOffset = stage.getX() - event.getScreenX();
+        yOffset = stage.getY() - event.getScreenY();
+    }
+    @FXML
+    protected void handleMovementAction(MouseEvent event) {
+        if (event.isPrimaryButtonDown()) {
+            Stage stage = (Stage) TopPane.getScene().getWindow();
+            stage.setX(event.getScreenX() + xOffset);
+            stage.setY(event.getScreenY() + yOffset);
+        }
+    }
+    @FXML
+    protected void chooseButtonAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        ExtensionFilter ex1 = new ExtensionFilter("Text files","*.txt");
+        ExtensionFilter ex2 = new ExtensionFilter("All files0","*.*");
+
+        fileChooser.getExtensionFilters().addAll(ex1,ex2);
+        fileChooser.setTitle("Choose file for sending");
+        fileChooser.setInitialDirectory(new File("C:\\Users\\Arvuti\\Desktop"));
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if(selectedFile != null) {
+            System.out.println("Open file");
+            System.out.println(selectedFile.getPath());
+            PathText.setText("File is attached");
+        }
+
+
+    }
+}
